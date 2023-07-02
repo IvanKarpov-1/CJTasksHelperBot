ef81d23161cea4159b8bafe04c8b659bfa62f2d7
Ivan Karpov <kivan8776@gmail.com>
2023-07-02 18:23:23 +0300

    feat: add medaitr logging behavior

--- CJTasksHelperBot.Application/ConfigureServices.cs
+++ CJTasksHelperBot.Application/ConfigureServices.cs
@@ -1,5 +1,8 @@
-﻿using CJTasksHelperBot.Application.Mapping;
+﻿using CJTasksHelperBot.Application.Common.Behaviours;
+using CJTasksHelperBot.Application.Common.Mapping;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace CJTasksHelperBot.Application;
 
@@ -8,6 +11,12 @@ public static class ConfigureServices
 	public static IServiceCollection AddApplicationServices(this IServiceCollection services)
 	{
 		services.AddScoped<MapperlyMapper>();
+
+		services.AddMediatR(cfg =>
+		{
+			cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+			cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+		});
 
 		return services;
 	}
