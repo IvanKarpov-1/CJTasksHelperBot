195b77c08adb8dbe4e5cce4bfdce3fda9be8b902
Ivan Karpov <kivan8776@gmail.com>
2023-07-04 14:47:06 +0300

    feat: add start command

---
+++ CJTasksHelperBot.Infrastructure/Commands/StartCommand.cs
@@ -0,0 +1,31 @@
+﻿using CJTasksHelperBot.Application.Common.Models;
+using CJTasksHelperBot.Infrastructure.Common.Interfaces;
+using Telegram.Bot;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace CJTasksHelperBot.Infrastructure.Commands;
+
+public class StartCommand : ICommand
+{
+	private readonly ITelegramBotClient _botClient;
+
+	public StartCommand(ITelegramBotClient botClient)
+	{
+		_botClient = botClient;
+	}
+
+	public CommandType.CommandType CommandType { get; set; } = Infrastructure.CommandType.CommandType.StartCommand;
+
+	public async Task ExecuteAsync(UserDto userDto, ChatDto chatDto, CancellationToken cancellationToken)
+	{
+		const string usage = "Використання:\n" +
+		                     "/start\t\t- показати доступні команди\n" +
+		                     "/get_current_user\t- отримати ім'я та фамілію поточного користувача\n";
+
+		await _botClient.SendTextMessageAsync(
+			chatId: chatDto.TelegramId,
+			text: usage,
+			replyMarkup: new ReplyKeyboardRemove(),
+			cancellationToken: cancellationToken);
+	}
+}
