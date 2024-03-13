b8a821cb635151fe8782165694af69d0ec0e699b
IvanKarpov-1 <kivan8776w@gmail.com>
2024-03-13 15:11:53 +0200

    refactor: extract database interaction logic
    make background service responsible only for notifications, and move database logic to the Application layer

---
+++ CJTasksHelperBot.Application/Task/Queries/GetSoonExpiredTasks.cs
@@ -0,0 +1,108 @@
+using CJTasksHelperBot.Application.Common.Interfaces;
+using CJTasksHelperBot.Application.Common.Mapping;
+using CJTasksHelperBot.Application.Common.Models;
+using CJTasksHelperBot.Domain.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TaskStatus = CJTasksHelperBot.Domain.Enums.TaskStatus;
+
+namespace CJTasksHelperBot.Application.Task.Queries;
+
+public class GetSoonExpiredTasksQuery : IRequest<Result<(List<IGrouping<long, GetTaskDto>>,
+    Dictionary<long, Dictionary<string, List<GetTaskDto>>>)>>
+{
+}
+
+public class GetSoonExpiredTasksQueryHandler : IRequestHandler<GetSoonExpiredTasksQuery, Result<(
+    List<IGrouping<long, GetTaskDto>>,
+    Dictionary<long, Dictionary<string, List<GetTaskDto>>>)>>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly MapperlyMapper _mapper;
+
+    public GetSoonExpiredTasksQueryHandler(IUnitOfWork unitOfWork, MapperlyMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+    public async
+        Task<Result<(List<IGrouping<long, GetTaskDto>>,
+            Dictionary<long, Dictionary<string, List<GetTaskDto>>>)>> Handle(GetSoonExpiredTasksQuery request,
+            CancellationToken cancellationToken)
+    {
+        var tasks = await _unitOfWork
+            .GetRepository<Domain.Entities.Task>()
+            .GetQueryable()
+            .Include(x => x.UserChat)
+            .ThenInclude(x => x!.Chat)
+            .Include(x => x.UserChat)
+            .ThenInclude(x => x!.User)
+            .Include(x => x.UserTaskStatuses)
+            .Where(
+                x => x.Deadline < DateTime.UtcNow.AddDays((int)x.NotificationLevel) &&
+                     x.Deadline > DateTime.UtcNow &&
+                     x.NotificationLevel != NotificationLevel.Never)
+            .ToListAsync(cancellationToken);
+
+        var groupedTasks = tasks
+            .GroupBy(x => new { x.UserChat!.User, x.UserChat.Chat })
+            .ToList();
+
+        var tasksPerGroup = groupedTasks
+            .SelectMany(grouping => grouping
+                .Select(task => new { grouping.Key.Chat!.Id, Task = _mapper.Map(task) }))
+            .GroupBy(item => item.Id, item => item.Task)
+            .ToList();
+
+        var flattenTasks = new Dictionary<long, List<(Domain.Entities.Task, TaskStatus)>>();
+        foreach (var grouping in groupedTasks)
+        {
+            foreach (var task in grouping)
+            {
+                foreach (var uts in task.UserTaskStatuses)
+                {
+                    if (flattenTasks.ContainsKey(uts.UserId) == false)
+                        flattenTasks[uts.UserId] = [];
+                    flattenTasks[uts.UserId].Add((task, uts.TaskStatus));
+                }
+            }
+        }
+
+        var tasksPerUser = new Dictionary<long, Dictionary<string, List<GetTaskDto>>>();
+        foreach (var (userId, usersTasks) in flattenTasks)
+        {
+            var groupsTasks = new Dictionary<string, List<GetTaskDto>>();
+
+            foreach (var tuple in usersTasks.Where(tuple => new[]
+                     {
+                         TaskStatus.DeadlineMissed,
+                         TaskStatus.Completed,
+                         TaskStatus.CompletedWithMissedDeadline
+                     }.Contains(tuple.Item2) == false))
+            {
+                var key = tuple.Item1.UserChat!.Chat!.Title ?? string.Empty;
+                if (groupsTasks.ContainsKey(key) == false)
+                    groupsTasks[key] = [];
+                groupsTasks[key].Add(_mapper.Map(tuple.Item1));
+            }
+
+            tasksPerUser[userId] = groupsTasks;
+        }
+
+        foreach (var task in tasks)
+        {
+            if (task.Deadline < DateTime.UtcNow.AddDays((int)NotificationLevel.Day))
+                task.SetNotificationLevel(NotificationLevel.Never);
+            else if (task.Deadline < DateTime.UtcNow.AddDays((int)NotificationLevel.TwoDays))
+                task.SetNotificationLevel(NotificationLevel.Day);
+            else
+                task.SetNotificationLevel();
+        }
+
+        await _unitOfWork.CommitAsync();
+
+        return Result<(List<IGrouping<long, GetTaskDto>>, Dictionary<long, Dictionary<string, List<GetTaskDto>>>)>
+            .Success((tasksPerGroup, tasksPerUser));
+    }
+}
--- CJTasksHelperBot.Infrastructure/Services/BackgroundServices/SoonExpiredTasksNotifierService.cs
+++ CJTasksHelperBot.Infrastructure/Services/BackgroundServices/SoonExpiredTasksNotifierService.cs
@@ -1,20 +1,19 @@
-using CJTasksHelperBot.Application.Common.Interfaces;
-using CJTasksHelperBot.Domain.Enums;
+using CJTasksHelperBot.Application.Common.Models;
+using CJTasksHelperBot.Application.Task.Queries;
 using CJTasksHelperBot.Infrastructure.Resources;
-using Microsoft.EntityFrameworkCore;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Logging;
 using Telegram.Bot;
 using Task = System.Threading.Tasks.Task;
-using TaskStatus = CJTasksHelperBot.Domain.Enums.TaskStatus;
 
 namespace CJTasksHelperBot.Infrastructure.Services.BackgroundServices;
 
 public class SoonExpiredTasksNotifierService : BackgroundService
 {
-    private readonly TimeSpan _period = TimeSpan.FromSeconds(15);
+    private readonly TimeSpan _period = TimeSpan.FromSeconds(1);
     private readonly IServiceProvider _serviceProvider;
     private readonly ILogger<SoonExpiredTasksNotifierService> _logger;
     private readonly ITelegramBotClient _botClient;
@@ -36,72 +35,22 @@ public class SoonExpiredTasksNotifierService : BackgroundService
             try
             {
                 await using var asyncScope = _serviceProvider.CreateAsyncScope();
-                var unitOfWork = asyncScope.ServiceProvider.GetRequiredService<IUnitOfWork>();
+                var mediator = asyncScope.ServiceProvider.GetRequiredService<IMediator>();
 
-                var tasks = await unitOfWork
-                    .GetRepository<Domain.Entities.Task>()
-                    .GetQueryable()
-                    .Include(x => x.UserChat)
-                    .ThenInclude(x => x!.Chat)
-                    .Include(x => x.UserChat)
-                    .ThenInclude(x => x!.User)
-                    .Where(x => x.Deadline < DateTime.UtcNow.AddDays((int)x.NotificationLevel) &&
-                        x.Deadline > DateTime.UtcNow &&
-                           x.NotificationLevel != NotificationLevel.Never)
-                    .ToListAsync(stoppingToken);
-
-                var groupedTasks = tasks
-                    .GroupBy(x => new { x.UserChat!.User, x.UserChat.Chat })
-                    .ToList();
-
-                var tasksPerGroup = groupedTasks
-                    .SelectMany(grouping => grouping
-                        .Select(task => new { grouping.Key.Chat, Task = task }))
-                    .GroupBy(item => item.Chat, item => item.Task)
-                    .ToList();
-
-                var tasksPerUser = groupedTasks
-                    .SelectMany(grouping => grouping
-                        .Select(task => new { grouping.Key.User, Task = task }))
-                    .GroupBy(item => item.User, item => item.Task)
-                    .ToList();
-
-                foreach ( var grouping in tasksPerGroup)
+                var (tasksPerGroup, tasksPerUser) = (await mediator.Send(new GetSoonExpiredTasksQuery(), stoppingToken)).Value;
+                
+                foreach (var grouping in tasksPerGroup)
                 {
-                    var group = grouping.Key;
+                    var groupId = grouping.Key;
                     var groupsTasks = grouping.ToList();
 
-                    await NotifyGroupAsync(group!.Id, groupsTasks, stoppingToken);
+                    await NotifyGroupAsync(groupId, groupsTasks, stoppingToken);
                 }
 
-                foreach (var grouping in tasksPerUser)
+                foreach (var (userId, tasksPerChat) in tasksPerUser)
                 {
-                    var user = grouping.Key;
-                    var usersTasks = grouping.ToList();
-
-                    var groupsTasks = new Dictionary<long, (string?, List<Domain.Entities.Task>)>();
-
-                    foreach (var task in usersTasks)
-                    {
-                        if (groupsTasks.ContainsKey(task.UserChat!.ChatId) == false)
-                            groupsTasks[task.UserChat.ChatId] = (task.UserChat.Chat!.Title, []);
-                        groupsTasks[task.UserChat.ChatId].Item2.Add(task);
-                    }
-
-                    await NotifyUserAsync(user!.Id, groupsTasks, stoppingToken);
+                    await NotifyUserAsync(userId, tasksPerChat, stoppingToken);
                 }
-
-                foreach (var task in tasks)
-                {
-                    if (task.Deadline < DateTime.UtcNow.AddDays((int)NotificationLevel.Day))
-                        task.SetNotificationLevel(NotificationLevel.Never);
-                    else if (task.Deadline < DateTime.UtcNow.AddDays((int)NotificationLevel.TwoDays))
-                        task.SetNotificationLevel(NotificationLevel.Day);
-                    else
-                        task.SetNotificationLevel();
-                }
-
-                await unitOfWork.CommitAsync();
             }
             catch (Exception ex)
             {
@@ -109,24 +58,33 @@ public class SoonExpiredTasksNotifierService : BackgroundService
             }
         }
     }
-
-    private async Task NotifyGroupAsync(long chatId, List<Domain.Entities.Task> tasks, CancellationToken cancellationToken)
+    
+    private async Task NotifyGroupAsync(long chatId, List<GetTaskDto> tasks, CancellationToken cancellationToken)
     {
         await Task.CompletedTask;
     }
     
-    private async Task NotifyUserAsync(long userId, Dictionary<long, (string?, List<Domain.Entities.Task>)> tasks, CancellationToken cancellationToken)
+    private async Task NotifyUserAsync(long userId, Dictionary<string, List<GetTaskDto>> tasks, CancellationToken cancellationToken)
     {
         foreach (var tuple in tasks)
         {
-            var title = tuple.Value.Item1 ?? _localizer["word_personal"];
+            var title = tuple.Key == string.Empty
+                ? _localizer["word_personal"]
+                : tuple.Key;
 
-            foreach (var task in tuple.Value.Item2)
+            foreach (var task in tuple.Value)
             {
-                await _botClient.SendTextMessageAsync(
-                    chatId: userId,
-                    text: $"{title}: {task.Title}",
-                    cancellationToken: cancellationToken);
+                try
+                {
+                    await _botClient.SendTextMessageAsync(
+                        chatId: userId,
+                        text: $"{title}: {task.Title}",
+                        cancellationToken: cancellationToken);
+                }
+                catch (Exception)
+                {
+                    // Ignore
+                }
             }
         }
     }
