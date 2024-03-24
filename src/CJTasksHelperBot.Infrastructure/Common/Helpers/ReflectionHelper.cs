10d96ef42bea928e670bb171228780e575d1c4ae
IvanKarpov-1 <kivan8776w@gmail.com>
2024-03-24 15:48:52 +0200

    refactor(infra): merge where conditions into pattern

--- src/CJTasksHelperBot.Infrastructure/Common/Helpers/ReflectionHelper.cs
+++ src/CJTasksHelperBot.Infrastructure/Common/Helpers/ReflectionHelper.cs
@@ -7,7 +7,7 @@ public static class ReflectionHelper
 	public static IEnumerable<T> GetInstance<T>()
 	{
 		var type = typeof(T);
-		var types = GetImplementationsOfType(type).Where(x => !x.IsAbstract && !x.IsInterface);
+		var types = GetImplementationsOfType(type).Where(x => x is { IsAbstract: false, IsInterface: false });
 		return types.Select(i => (T)Activator.CreateInstance(i)!).ToList();
 	}
 
@@ -15,7 +15,7 @@ public static class ReflectionHelper
 	{
 		return Assembly.GetAssembly(type)!
 			.GetTypes()
-			.Where(x => type.IsAssignableFrom(x) && x != type && !x.IsAbstract && !x.IsInterface)
+			.Where(x => type.IsAssignableFrom(x) && x != type && x is { IsAbstract: false, IsInterface: false })
 			.ToList();
 	}
 }
